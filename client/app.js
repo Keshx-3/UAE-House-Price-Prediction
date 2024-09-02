d9e04bc0d777ee58ce4464a77cfbea54aa8b4952
Kesavadas <kesavadaskesh@gmail.com>
2024-09-02 10:21:46 +0400

    Update app.js

--- client/app.js
+++ client/app.js
@@ -26,13 +26,13 @@ function onClickedEstimatePrice() {
   var location = document.getElementById("uiLocations");
   var estPrice = document.getElementById("uiEstimatedPrice");
 
-  var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
-  //var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+  var url = "http://127.0.0.1:5000/predict_home_price"; 
+ 
 
   $.post(url, {
     squarefeet: parseFloat(sqft.value),
     bhk: bhk,
-    bathrooms: bathrooms,  // Changed from 'bath' to 'bathrooms'
+    bathrooms: bathrooms,  
     location: location.value
 }, function(data, status) {
     console.log(data.estimated_price);
@@ -44,8 +44,7 @@ function onClickedEstimatePrice() {
 
 function onPageLoad() {
   console.log( "document loaded" );
-  var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
-  //var url = "/api/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+  var url = "http://127.0.0.1:5000/get_location_names"; 
   $.get(url,function(data, status) {
       console.log("got response for get_location_names request");
       if(data) {
